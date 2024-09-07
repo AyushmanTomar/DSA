7506db349b532307e835681db7f6d65480ebecf7
Ayushman Tomar <141900459+AyushmanTomar@users.noreply.github.com>
2024-09-08 00:27:28 +0530

    update palindrome ll

--- double ll/creation_dll.cpp
+++ double ll/creation_dll.cpp
@@ -1,25 +1,32 @@
 #include<iostream>
 using namespace std;
-class Node{
-    int data;
-    Node *next;
-    Node *prev;
-    Node(int data1, Node *next1, Node *prev1)
-    {
-        data = data1;
-        next = next1;
-        prev = prev1;
-    }
-    Node(int data1,Node *next1)
-    {
-        data = data1;
-        next = next1;
-        prev = nullptr;
-    }
-    Node(int data1)
-    {
-        data = data1;
-        next = nullptr;
-        prev = nullptr;
-    }
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node *prev;
+        Node(int data1, Node *next1, Node *prev1)
+        {
+            data = data1;
+            next = next1;
+            prev = prev1;
+        }
+        Node(int data1,Node *next1)
+        {
+            data = data1;
+            next = next1;
+            prev = nullptr;
+        }
+        Node(int data1)
+        {
+            data = data1;
+            next = nullptr;
+            prev = nullptr;
+        }
 };
+int main()
+{
+    Node *node = new Node(5);
+    return 0;
+}
